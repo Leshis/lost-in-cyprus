7435b180694fb3d1d9996cafc93c9438e23ddae3
Leshis <94430516+Leshis@users.noreply.github.com>
2026-04-20 14:10:53 +0100

    Update supabase.js
    Update Url and Key

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Get these from your Supabase Settings > API
 const supabaseUrl = 'https://bwxqxgjwpsurptaduwii.supabase.co'
-const supabasePublishableKey = 'sb_publishable_9PZOHIJ2nWUT9f1ZzNI2Bw__pgRAVTs'
+const supabasePublishableKey = 'sb_publishable_9PZOHlJ2nWUT9f1ZzNI2Bw__pgRAVTs'
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
